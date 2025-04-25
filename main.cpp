540c7c6d1b63e392472841a516ae91400860cb75
josippolic <128246657+josippolic@users.noreply.github.com>
2025-04-25 11:55:28 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const string FILE_NAME = "passwords.txt";
+const string WORDLIST_PATH = "/usr/share/wordlists/wifilite.txt";
+const string CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@Ł!&#$/<>*ł?£&{}[]€;:-_÷^";
+
+// Učitaj prethodno generirane lozinke s brojačem
+unordered_map<string, int> readPasswords() {
+    unordered_map<string, int> passwords;
+    ifstream file(FILE_NAME);
+    if (file.is_open()) {
+        string password;
+        int count;
+        while (file >> password >> count) {
+            passwords[password] = count;
+        }
+        file.close();
+    }
+    return passwords;
+}
+
+// Zapiši sve lozinke i broj generiranja u datoteku
+void writePasswords(const unordered_map<string, int>& passwords) {
+    ofstream file(FILE_NAME);
+    if (file.is_open()) {
+        for (const auto& entry : passwords) {
+            file << entry.first << " " << entry.second << "\n";
+        }
+        file.close();
+    }
+}
+
+// Učitaj wordlist i zapamti sve lozinke koje se ne smiju koristiti
+unordered_set<string> readWordlist(const string& path) {
+    unordered_set<string> blacklist;
+    ifstream file(path);
+    if (file.is_open()) {
+        string word;
+        while (getline(file, word)) {
+            blacklist.insert(word);
+        }
+        file.close();
+    }
+    return blacklist;
+}
+
+// Generiraj slučajnu lozinku
+string generatePassword(int length) {
+    string password;
+    for (int i = 0; i < length; i++) {
+        password += CHARACTERS[rand() % CHARACTERS.length()];
+    }
+    return password;
+}
+
+// Glavna funkcija aplikacije
+void passwordGenerator() {
+    srand(time(0));
+    unordered_map<string, int> passwords = readPasswords();
+    unordered_set<string> blacklist = readWordlist(WORDLIST_PATH);
+
+    while (true) {
+        cout << "\n================== Password Generator App ==================\n\n";
+
+        int passlen;
+        cout << "Enter the length of password (8-15): ";
+        cin >> passlen;
+
+        if (cin.fail() || passlen < 8 || passlen > 15) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "\nPassword length should be between 8 and 15 characters.\n";
+            continue;
+        }
+
+        string password;
+        // Generiraj dok ne dobijemo jedinstvenu lozinku (nije u wordlisti)
+        do {
+            password = generatePassword(passlen);
+        } while (blacklist.find(password) != blacklist.end());
+
+        if (passwords.find(password) != passwords.end()) {
+            passwords[password]++;
+            cout << "\n\nYour password is: " << password;
+            cout << "\nThis password has been generated " << passwords[password] << " times.";
+        } else {
+            passwords[password] = 1;
+            cout << "\n\nYour password is: " << password;
+        }
+
+        writePasswords(passwords);
+
+        if (passlen == 15) {
+            cout << "\nWarning: Password should not have 15 or more characters.\n";
+        }
+
+        string opt;
+        cout << "\nDo you want to try again? (da/ne): ";
+        cin >> opt;
+
+        if (opt == "ne") {
+            cout << "Exiting program....\n";
+            break;
+        } else if (opt != "da") {
+            cout << "Please enter da/ne:\n";
+        }
+    }
+}
+
+int main() {
+    passwordGenerator();
+    return 0;
+}
